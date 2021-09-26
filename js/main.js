fb243dd44f4c73ae5a1592d824b69c18c82ca4ce
Adam Millar <adammillar@Adams-MacBook-Air.local>
2021-09-26 16:11:25 -0700

    Move Cell class to main.js. Make init function for board array. Update Cell class to set all properties to false.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,46 @@
 /*----- constants -----*/
+let boardWidth = 4;
+let boardHeight = 4;
+
+
 
 /*----- app's state (variables) -----*/
+//for each cell, tracking whether it is mined, 
+// Board object and each of the cells. 
+class Cell{
+    constructor(){
+        this.isMined = false; //should be a bool
+        this.isShown = false; //also a bool
+        this.isFlagged = false; //yet another bool
+        this.adjSum = foo; //This one will be quite a bit of code
+
+    } 
+    show() {
+        this.isShown = true;
+    }
+    calcAdj() {
+        
+    }
+ }
 
 /*----- cached element references -----*/
+let board = document.querySelectorAll()
 
 /*----- event listeners -----*/
+//click on cell and ctrl-click on cell
 
 /*----- functions -----*/
+init() ;
+
+function init() {
+    board = [];
+  for (let i = 0; i <= boardWidth; i++) {
+      for (let j = 0; j <= boardHeight; j++) ;
+            let newCell = new Cell;
+;           
+      };
+      
+  }  
+} 
+render() ;
+

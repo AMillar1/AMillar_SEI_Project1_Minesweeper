15d2a4cb887b5fe947735e541beb13a2c4203655
Adam Millar <adammillar@Adams-MacBook-Air.local>
2021-09-27 14:47:11 -0700

    Write first attempt at placeMine function

--- js/main.js
+++ js/main.js
@@ -9,20 +9,21 @@ let board;
 //for each cell, tracking whether it is mined, 
 // Board object and each of the cells. 
 class Cell{
-    constructor(index1, index2){
+    constructor(row, col){
         this.isMined = false; //should be a bool
         this.isShown = false; //also a bool
         this.isFlagged = false; //yet another bool
         //this.adjSum = foo; //This one will be quite a bit of code
-        this.index1 = index1;
-        this.index2 = index2;
-
+        this.row = row;
+        this.col = col;
     } 
     show() {
         this.isShown = true;
     }
-    calcAdj() {
-        
+    calcAdj() {    
+    }
+    placeMine(){
+        this.isMined = true;
     }
  }
 
@@ -39,23 +40,28 @@ function init() {
     for (let row = 0; row < boardWidth; row++) {
         board.push([]);  // add row array
         for (let col = 0; col < boardHeight; col++) {
-              let newCell = new Cell(row, col)
-              board[row].push[newCell];
-         }  // no semicolon at end of code blocks
+              let newCell = new Cell(row, col);
+              board[row].push(newCell);
+         }  
       }
+// now to randomly mine some cells. 
+    let numMines = 1;
+    while (numMines > 0) {
+        let randCol = Math.floor(Math.random()) * 4;
+        let randRow = Math.floor(Math.random()) * 4;
+        if (board[randRow][randCol].isMined === false) {
+            board[randRow][randCol].isMined = true;
+            numMines--;
+        }
+     }
   }  // note there's no semicolon at end of fn declarations
   console.log(board);
 
-// function init() {
-//     board = [];
-//   for (let i = 0; i < boardWidth; i++) {
-//       for (let j = 0; j < boardHeight; j++) {
-//             let newCell = new Cell(i, j)
-//             board.push[newCell];
-//         };
-//     };     
-// };
-//render() ;
+  new Cell(1,2);
+  console.log()
+
+
+
 
 
 

45059917a54bcadf5b155e501b695ee1f65d8540
Adam Millar <adammillar@Adams-MacBook-Air.local>
2021-09-27 16:54:07 -0700

    Complete mining cells function. Attempt render board function

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ let board;
 class Cell{
     constructor(row, col){
         this.isMined = false; //should be a bool
-        this.isShown = false; //also a bool
+        this.isShown = true; //also a bool
         this.isFlagged = false; //yet another bool
         //this.adjSum = foo; //This one will be quite a bit of code
         this.row = row;
@@ -48,11 +48,19 @@ function init() {
 // now to randomly mine some cells. 
     let numMines = 7;
     while (numMines > 0) {
-        let randCol = Math.floor(Math.random() * 4);
-        let randRow = Math.floor(Math.random() * 4);
+        let randCol = Math.floor(Math.random() * boardHeight);
+        let randRow = Math.floor(Math.random() * boardWidth);
         if (board[randRow][randCol].isMined === false) {
             board[randRow][randCol].isMined = true;
             numMines--;
      }
   }  // note there's no semicolon at end of fn declarations
   console.log(board);
+
+// function renderBoard() {
+//     board.forEach(function(colArr, row){
+//         colArr.forEach(cell, col) {
+//             const cellDiv = foo;
+//         }
+//     });
+// }

ba19d2e0b00617e77eee7353f4534df94e86f220
Adam Millar <adammillar@Adams-Air.hsd1.wa.comcast.net>
2021-09-30 12:38:12 -0700

    Apply input from Jim. Refactor references to row/col index order. Remove comparisons of booleans. Spruce up init and render functions. Implement css classes for cell states. Stub show() method for cell class.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ let board;
 
 
 
+
 /*----- app's state (variables) -----*/
 //for each cell, tracking whether it is mined, 
 // Board object and each of the cells. 
@@ -19,8 +20,19 @@ class Cell {
     }
     show() {
         this.isShown = true;
+        if (this.isMined) {
+            winnerOrLose = 'L';
+            for (let row = 0; row < boardHeight; row++) {
+                for (let col = 0; col < boardWidth; col++) {
+                    board[row][col].isShown = true;
+                }
+            }
+        } 
+    
     }
 }
+let cellEls;
+let winnerOrLose; 
 
 /*----- cached element references -----*/
 const boardEl = document.getElementById('board');
@@ -30,12 +42,13 @@ const boardEl = document.getElementById('board');
 /*----- event listeners -----*/
 document.getElementById('board').addEventListener('click', handleLeftClick);
 document.getElementById('board').addEventListener('contextmenu', handleRightClick);
+
 //click on cell and ctrl-click on cell
 
 init();
 /*----- functions -----*/
 function addMines() {
-    let numMines = 8;
+    let numMines = 12;
     while (numMines > 0) {
         let randCol = Math.floor(Math.random() * boardHeight); //generate a random col index
         let randRow = Math.floor(Math.random() * boardWidth); //generate a random row index
@@ -46,74 +59,73 @@ function addMines() {
 }
 
 function init() {
+    winnerOrLose = null; 
     board = [];
-    for (let row = 0; row < boardWidth; row++) {
+    for (let row = 0; row < boardHeight; row++) {
         board.push([]);  // add row array
-        for (let col = 0; col < boardHeight; col++) {
+        for (let col = 0; col < boardWidth; col++) {
             let newCell = new Cell(row, col);
             board[row].push(newCell); //this creates the disired JS object 'board,' a 2D array with cell objects as base elements. 
         }
     }
     addMines();
-    for (let row = 0; row < boardWidth; row++) {
-        for (let col = 0; col < boardHeight; col++) {
+    for (let row = 0; row < boardHeight; row++) {
+        for (let col = 0; col < boardWidth; col++) {
             const cellDiv = document.createElement('div');
             cellDiv.id = `c${col}r${row}`;
             boardEl.appendChild(cellDiv);
-            if (board[row][col].isMined === true) {
-                cellDiv.classList.add('mine')
-            }
         }
     }
     calcAdj();
+    cellEls = [...document.querySelectorAll('#board > div')];
+    render();
 }
-const cellEls = [...document.querySelectorAll('#board > div')];
+
 
 
 function calcAdj() {
-    for (let row = 0; row < boardWidth; row++) {
-        for (let col = 0; col < boardHeight; col++) {//for every cell in the board array
+    for (let row = 0; row < boardHeight; row++) {
+        for (let col = 0; col < boardWidth; col++) {//for every cell in the board array
             let counter = 0;
             // if (!board[row + 1] || !board[row + 1][col + 1] || !board[row - 1]) break;
-            if (row < boardHeight - 1 && col < boardWidth - 1 && board[row + 1][col + 1].isMined === true) {//check SE extra conditions are for edge cells. All cells of the form 
+            if (row < boardHeight - 1 && col < boardWidth - 1 && board[row + 1][col + 1].isMined) {//check SE extra conditions are for edge cells. All cells of the form 
                 counter++;//increase the counter
             }
-            if (row < boardHeight - 1 && col > 0 && board[row + 1][col - 1].isMined === true) {// check SW 
+            if (row < boardHeight - 1 && col > 0 && board[row + 1][col - 1].isMined) {// check SW 
                 counter++;
             }
-            if (row > 0 && col < boardWidth - 1 && board[row - 1][col + 1].isMined === true) {//  check NE
+            if (row > 0 && col < boardWidth - 1 && board[row - 1][col + 1].isMined) {//  check NE
                 counter++;
             }
-            if (col > 0 && row > 0 && board[row - 1][col - 1].isMined === true) {// check NW
+            if (col > 0 && row > 0 && board[row - 1][col - 1].isMined) {// check NW
                 counter++;
             }
-            if (col < boardWidth - 1 && board[row][col + 1].isMined === true) {// check E
+            if (col < boardWidth - 1 && board[row][col + 1].isMined) {// check E
                 counter++;
             }
-            if (col > 0 && board[row][col - 1].isMined === true) {//check W
+            if (col > 0 && board[row][col - 1].isMined) {//check W
                 counter++;
             }
-            if (row > 0 && board[row - 1][col].isMined === true) {//check N
+            if (row > 0 && board[row - 1][col].isMined) {//check N
                 counter++;
             }
-            if (row < boardHeight - 1 && board[row + 1][col].isMined === true) {// check S
+            if (row < boardHeight - 1 && board[row + 1][col].isMined) {// check S
                 counter++;
             }
             // document.getElementById(`c${col}r${row}`).innerText = counter; //put in innerHTML and toggle only that <h1>
-            board[col][row].adjSum = counter; 
+            board[row][col].adjSum = counter;
         }
     }
 }
 function handleLeftClick(evt) {
-    const index = cellEls.indexOf(evt.target);
-    let colIdx = index % boardWidth;
-    let rowIdx = Math.floor(index / boardHeight);
-    console.log(colIdx, rowIdx);
-    if (index === -1) return;
-    if (board[colIdx][rowIdx].isShown === false) {
-        board[colIdx][rowIdx].isShown = true;
+    const id = evt.target.id;
+    let colIdx = parseInt(id[1]);
+    let rowIdx = parseInt(id[3]);
+    if (id === 'board') return;
+    if (!board[rowIdx][colIdx].isShown) {
+        board[rowIdx][colIdx].show();
     }
-    console.log(board);
+    console.log(colIdx, rowIdx);
     render();
 }
 function handleRightClick(evt) {
@@ -125,7 +137,6 @@ function handleRightClick(evt) {
     if (board[colIdx][rowIdx].isFlagged === false) {
         board[colIdx][rowIdx].isFlagged = true;
     }
-    console.log(board);
     render();
 }
 
@@ -136,17 +147,20 @@ function render() {
 }
 
 function renderBoard() {
-    cellEls.forEach(function(cell) {
-        let index = cellEls.indexOf(cell);
-        let domCell = board[index % boardWidth][Math.floor(index / boardHeight)];
-        if (domCell.isFlagged === true) {
-            cell.classList.add('flag');
-        }    
-        if (domCell.isShown && !domCell.isMined) {
-            cell.innerText = domCell.adjSum === 0 ? '' : domCell.adjSum;
-            console.log(cell);
-            console.log(domCell.adjSum);
+    cellEls.forEach(function (cellEl) {
+        let id = cellEl.id;
+        let colIdx = parseInt(id[1]);
+        let rowIdx = parseInt(id[3]);
+        let cell = board[rowIdx][colIdx];
+        if (cell.isFlagged) {
+            cellEl.className = 'flag';
+        } else if (cell.isShown && !cell.isMined) {
+            cellEl.innerText = (cell.adjSum === 0 ? '' : cell.adjSum);
+            cellEl.className = 'shown';
+        } else if (cell.isShown && cell.isMined) {
+            cellEl.className = 'mine';
+        } else {
+            cellEl.className = 'covered';
         }
-    })
+    });
 }
-console.log(cellEls);
